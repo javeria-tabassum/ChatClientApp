42d7f55f270b7a12ae5146c1979320d5384c042f
javeria.tabassum <javeria.tabassum@DEV-JAVERIA>
2024-09-05 17:10:47 +0500

    Updated the libs

--- MainForm.cs
+++ MainForm.cs
@@ -3,12 +3,12 @@ using NetMQ;
 using NetMQ.Sockets;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Configuration;
 
 namespace ChatClientApp
 {
@@ -350,4 +350,3 @@ namespace ChatClientApp
         }
     }
 }
- 
